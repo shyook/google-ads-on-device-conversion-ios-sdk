3fb9526fc8465ae0adc9ea8e2ba8d0b7700a607a
shyook <33274284+shyook@users.noreply.github.com>
2025-08-01 14:11:15 +0900

    Add files via upload
    init

---
+++ Package.swift
@@ -0,0 +1,66 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import PackageDescription
+
+let package = Package(
+  name: "GoogleAdsOnDeviceConversion",
+  platforms: [.iOS(.v12), .macOS(.v10_15), .tvOS(.v13), .watchOS(.v7)],
+  products: [
+    .library(
+      name: "GoogleAdsOnDeviceConversion",
+      targets: ["GoogleAdsOnDeviceConversionTarget"]
+    ),
+  ],
+  dependencies: [
+    .package(
+      name: "GoogleUtilities",
+      url: "https://github.com/google/GoogleUtilities.git",
+      "8.1.0" ..< "9.0.0"
+    ),
+    .package(
+      name: "nanopb",
+      url: "https://github.com/firebase/nanopb.git",
+      "2.30910.0" ..< "2.30911.0"
+    ),
+  ],
+  targets: [
+    .target(
+      name: "GoogleAdsOnDeviceConversionTarget",
+      dependencies: [
+        .target(
+          name: "GoogleAdsOnDeviceConversion",
+          condition: .when(platforms: [.iOS])
+        ),
+        .product(name: "GULLogger", package: "GoogleUtilities"),
+        .product(name: "GULNetwork", package: "GoogleUtilities"),
+        .product(name: "nanopb", package: "nanopb"),
+      ],
+      path: "GoogleAdsOnDeviceConversionWrapper",
+      linkerSettings: [
+        .linkedLibrary("c++"),
+      ]
+    ),
+    .binaryTarget(
+      name: "GoogleAdsOnDeviceConversion",
+      url: "https://dl.google.com/firebase/ios/appads/2.2.0/GoogleAdsOnDeviceConversion.zip",
+      checksum: "95052dc8590cd2df53e169a515e06a6ae87fa0a570d31e23cf79252e21975a18"
+    ),
+  ],
+  cLanguageStandard: .c99,
+  cxxLanguageStandard: CXXLanguageStandard.gnucxx14
+)
---
+++ README.md
@@ -0,0 +1,6 @@
+# GoogleAdsOnDeviceConversion
+This repo hosts the GoogleAdsOnDeviceConversion Swift Package Manager distribution.
+
+GoogleAdsOnDeviceConversion only supports Google and Google's App Attribution Partner (AAP) libraries. This update will incorporate additional de-identified, temporary signals, such as data derived from IP-address-based attributes, to improve iOS campaign optimization and reporting in third-party App Attribution Partner interfaces - all while continuing to protect user privacy. Any issues should be reported to the product that is using GoogleAdsOnDeviceConversion.
+
+As a reminder, you have the option to display a prominent link in your disclosures to make clear how Google uses information from sites or apps that use our services, including information about the technologies used.
